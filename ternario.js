ddb1cc4f05faf470e321242eb6b96791a3678581
henriquemartinsr <henrique.martinsr@gmail.com>
2025-04-25 06:57:34 -0300

    ternarios

---
+++ ternario.js
@@ -0,0 +1,10 @@
+let idade = 18;
+let mensagem;
+
+idade >= 18 ? mensagem = 'Maior de idade'  : mensagem = 'Menor de idade';
+console.log(mensagem);
+
+let numero = -5;
+
+let resultado = numero > 0 ? 'Positivo' : (numero < 0 ? 'Negativo' : 'Zero');
+console.log(resultado)
